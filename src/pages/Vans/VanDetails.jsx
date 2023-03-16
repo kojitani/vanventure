d4b71832b39f5c6b2770d65cc011b2181620743c
Koji Taniguchi <64213053+kojitani@users.noreply.github.com>
2023-03-16 19:34:40 +0900

    testing mobile booking

--- src/pages/Vans/VanDetails.jsx
+++ src/pages/Vans/VanDetails.jsx
@@ -17,10 +17,10 @@ export default function VanDetails() {
     console.log('clicked');
   }
   function openMobileBooking() {
+    document.querySelector('body').classList.add('body-hide-overflow');
     document.querySelector('.mantine-1avyp1d').style.display = 'none';
     document.querySelector('.mobile-booking-modal').style.display = 'flex';
     document.querySelector('.mobile-booking-container').style.display = 'none';
-    document.querySelector('body').classList.add('body-hide-overflow');
   }
   return (
     <div className="fallback-container hidden" id="fallback-container">
